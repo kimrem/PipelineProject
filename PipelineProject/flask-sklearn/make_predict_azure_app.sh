c2f35c8e59d77be759ffe6573b0685f4294605c3
Kim <kim-remmers@web.de>
2022-08-21 14:05:09 +0200

    update with App name

--- PipelineProject/flask-sklearn/make_predict_azure_app.sh
+++ PipelineProject/flask-sklearn/make_predict_azure_app.sh
@@ -25,5 +25,6 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://pipelineprojectwebapp.azurewebsites.net:$PORT/predict
+     
      #your application name <yourappname>goes here
