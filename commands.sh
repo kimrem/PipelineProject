8ca1a83a8edcc302a06b771c48e8654f6ee51340
kimrem <kim-remmers@web.de>
2022-09-01 21:14:46 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,7 @@
 git clone git@github.com:kimrem/PipelineProject.git
 cd PipelineProject
+python3 -m venv ~/.flask-ml-service
+source ~/.flask-ml-service/bin/activate
 git pull
 make all
 az webapp up --name myWebAppKR12 --resource-group Azuredevops --runtime "PYTHON:3.7"
